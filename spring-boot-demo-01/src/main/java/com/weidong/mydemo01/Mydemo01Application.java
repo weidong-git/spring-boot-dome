0b352b355ca867850fc9d424d7b15c6ac9860159
张伟东 <1130160475@qq.com>
2017-11-06 23:44:07 +0800

    spring boot 访问方式

---
+++ spring-boot-demo-01/src/main/java/com/weidong/mydemo01/Mydemo01Application.java
@@ -0,0 +1,12 @@
+package com.weidong.mydemo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mydemo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mydemo01Application.class, args);
+	}
+}
---
+++ spring-boot-demo-01/src/main/java/com/weidong/mydemo01/controller/IndexController.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (C), 2017-2017, 1130160475@qq.com
+ * FileName: IndexController
+ * Author:   ${1130160475@qq.com}
+ * Date:     2017/11/6 0006 下午 11:36
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.weidong.mydemo01.controller;
+
+import com.weidong.mydemo01.bean.User;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.HashMap;
+
+@RestController
+
+public class IndexController {
+    @RequestMapping
+    public String index() {
+        return "hello world";
+    }
+
+    // @RequestParam 简单类型的绑定，可以出来get和post
+    @RequestMapping(value = "/get")
+    public HashMap<String, Object> get(@RequestParam String name) {
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("title", "hello world");
+        map.put("name", name);
+        return map;
+    }
+
+    // @PathVariable 获得请求url中的动态参数
+    @RequestMapping(value = "/get/{id}/{name}")
+    public User getUser(@PathVariable int id, @PathVariable String name) {
+        User user = new User();
+        user.setId(id);
+        user.setName(name);
+        user.setDate(new Date());
+        return user;
+    }
+}
