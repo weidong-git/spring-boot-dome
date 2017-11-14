7280c2c3b98107ec7ba129ddae7bd04ab9aca6d9
张伟东 <1130160475@qq.com>
2017-11-15 00:21:18 +0800

    自定义配置文件（日志文件配置）

---
+++ spring-boot-dome-04/src/main/java/com/weidong/myspring/SpringBootDome04Application.java
@@ -0,0 +1,12 @@
+package com.weidong.myspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDome04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDome04Application.class, args);
+	}
+}
